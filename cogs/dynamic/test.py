b14ff338ecce133c3b90dcbb0acb9486087e87e9
EcstasyEngineer <108830997+EcstasyEngineer@users.noreply.github.com>
2025-08-08 09:54:28 -0500

    eto bleh

--- cogs/dynamic/test.py
+++ cogs/dynamic/test.py
@@ -8,17 +8,17 @@ class Test(commands.Cog):
         self.logger = bot.logger
 
 
-@commands.command(name="getconfig")
-async def get_config(ctx):
-    """Get the current configuration."""
-    config = ctx.bot.config
-    await ctx.send(f"Current config: {config}")
+    @commands.command(name="getconfig")
+    async def get_config(ctx):
+        """Get the current configuration."""
+        config = ctx.bot.config
+        await ctx.send(f"Current config: {config}")
 
 
-@commands.command(name="helloworld")
-async def hello_world(ctx):
-    """A simple command that responds with 'Hello, World!'."""
-    await ctx.send("Hello, World!")
+    @commands.command(name="helloworld")
+    async def hello_world(ctx):
+        """A simple command that responds with 'Hello, World!'."""
+        await ctx.send("Hello, World!")
 
 async def setup(bot):
     """Every cog needs a setup function like this."""
